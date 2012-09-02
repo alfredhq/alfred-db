5c0f95b1a21abb10ed5156c7aa8195bcfadc5c9b
Dima Kukushkin <dima@kukushkin.me>
2012-09-02 12:48:32 +0400

    Add User-Organization many to many association

--- alfred_db/models/organization.py
+++ alfred_db/models/organization.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from .base import Base
 
@@ -10,7 +11,24 @@ class Organization(Base):
     login = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
 
+    users = relationship('User', secondary='memberships',
+                         backref='organizations')
+
     __tablename__ = 'organizations'
 
     def __repr__(self):
         return '<Organization({!r})>'.format(self.login)
+
+
+class Membership(Base):
+
+    id = Column(Integer, primary_key=True)
+    organization_id = Column(Integer, ForeignKey('organizations.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    __tablename__ = 'memberships'
+
+    def __repr__(self):
+        return '<Membership(<User({!r})>-<Organization({!r})>)'.format(
+            self.organization_id, self.user_id
+        )
