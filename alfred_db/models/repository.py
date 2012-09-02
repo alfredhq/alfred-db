6ff739884516787f93cb0e45322dda3760d23e0b
Dima Kukushkin <dima@kukushkin.me>
2012-09-02 15:28:36 +0400

    Change Repository: owner can be user or organization

--- alfred_db/models/repository.py
+++ alfred_db/models/repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, UniqueConstraint
+from sqlalchemy import Column, Integer, String, UniqueConstraint, Enum
 from .base import Base
 
 
@@ -7,12 +7,15 @@ class Repository(Base):
     id = Column(Integer, primary_key=True)
     url = Column(String, nullable=False)
     name = Column(String, nullable=False)
-    user = Column(String, nullable=False)
+    owner_name = Column(String, nullable=False)
+    owner_type = Column(Enum('organization', 'user', native_enum=False),
+                        nullable=False)
+    owner_id = Column(Integer, nullable=False)
 
     __tablename__ = 'repositories'
     __table_args__ = (
-        UniqueConstraint(user, name),
+        UniqueConstraint(owner_id, owner_type),
     )
 
     def __repr__(self):
-        return '<Repository({!r}, {!r})>'.format(self.user, self.name)
+        return '<Repository({!r}, {!r})>'.format(self.owner_name, self.name)
