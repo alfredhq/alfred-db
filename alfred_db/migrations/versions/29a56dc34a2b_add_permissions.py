65fcd98e65a5921dabf324e82a5e5925b1279a30
Dima Kukushkin <dima@kukushkin.me>
2012-09-03 22:34:42 +0400

    Fix permission table creation migration

--- alfred_db/migrations/versions/29a56dc34a2b_add_permissions.py
+++ alfred_db/migrations/versions/29a56dc34a2b_add_permissions.py
@@ -7,7 +7,7 @@ Create Date: 2012-09-02 14:06:24.088307
 
 # revision identifiers, used by Alembic.
 revision = '29a56dc34a2b'
-down_revision = '4fdf1059c4ba'
+down_revision = '5245d0b46f8'
 
 
 from alembic import op
@@ -22,8 +22,12 @@ def upgrade():
         sa.Column('admin', sa.Boolean(), nullable=False),
         sa.Column('push', sa.Boolean(), nullable=False),
         sa.Column('pull', sa.Boolean(), nullable=False),
-        sa.ForeignKeyConstraint(['repository_id'], ['repositories.id'], ),
-        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+        sa.ForeignKeyConstraint(
+            ['repository_id'], ['repositories.id'], ondelete='CASCADE',
+        ),
+        sa.ForeignKeyConstraint(
+            ['user_id'], ['users.id'], ondelete='CASCADE',
+        ),
         sa.PrimaryKeyConstraint('id')
     )
 
